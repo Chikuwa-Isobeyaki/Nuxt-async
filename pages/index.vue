15822331c478514d92d6053ab59471004586065b
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-13 20:53:48 +0900

    [Fix]v-forを使ってusers情報を表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="container">
-      {{ users[0].id }}, {{ users[0].name }}
+      <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <ul>
+        <!-- データを追跡しやすいようにv-for:keyを設定。公式の方でも指定を推奨されている -->
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}
+        </li>
+      </ul>
   </section>
 </template>
 
